beb624c05a254db6ce02ce8bca30e1967404549a
Fan1asyHX <enrico0541@gmail.com>
2025-04-18 22:26:29 +0800

    Site updated: 2025-04-18 22:26:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/11/Pastelic-256x-V2-Purple/","2025/04/11/hello-world/"];function toRandomPost(){
+var posts=["2025/04/11/Pastelic-256x-V2-Purple/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
