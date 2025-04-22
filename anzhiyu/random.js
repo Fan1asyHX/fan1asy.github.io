81b5e1ac0a3f7fb2437a9182fb0d396271101752
Fan1asyHX <enrico0541@gmail.com>
2025-04-22 14:40:27 +0800

    Site updated: 2025-04-22 14:40:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/11/Pastelic-256x-V2-Purple/"];function toRandomPost(){
+var posts=["2025/04/11/Pastelic-256x-V2-Purple/","2025/04/11/Stardust [256x] Sunset by PackFolder/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
