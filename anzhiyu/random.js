25f998668c3fa56efb671f84510046f23d120dc7
Fan1asyHX <enrico0541@gmail.com>
2025-05-07 20:19:04 +0800

    Site updated: 2025-05-07 20:19:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/02/Nino Nakano [32x]/","2025/04/22/Stardust [256x] Sunset by PackFolder/","2025/04/11/Pastelic-256x-V2-Purple/"];function toRandomPost(){
+var posts=["2025/05/02/Nino_Nakano_[32x]/","2025/04/22/Stardust_[256x]_Sunset/","2025/04/11/Pastelic_256x_V2_Purple/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
