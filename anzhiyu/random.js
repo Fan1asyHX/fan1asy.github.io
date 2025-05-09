045d2d9adb661d3618585cbdc3fef16623b9582c
Fan1asyHX <enrico0541@gmail.com>
2025-05-09 16:41:21 +0800

    Site updated: 2025-05-09 16:41:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/08/ColeUnity_35k/","2025/05/02/Nino_Nakano_32x/","2025/04/11/Pastelic_256x_V2_Purple/","2025/04/22/Stardust_256x_Sunset/","2025/05/07/光影包/","2025/05/07/模组包/"];function toRandomPost(){
+var posts=["2025/05/09/Alya-x16/","2025/05/08/ColeUnity_35k/","2025/05/02/Nino_Nakano_32x/","2025/04/11/Pastelic_256x_V2_Purple/","2025/04/22/Stardust_256x_Sunset/","2025/05/07/光影包/","2025/05/07/模组包/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
